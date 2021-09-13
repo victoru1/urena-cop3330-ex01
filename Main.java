bbac8ac1636cbe0366f06ea0e84d713ec4494a08
victoru1 <90523541+victoru1@users.noreply.github.com>
2021-09-13 00:48:07 -0400

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    var scanner = new java.util.Scanner(System.in);
-    System.out.print("What is your name? ");
-    var name = scanner.nextLine();
-    System.out.println("Hello, " + name + ", nice to meet you!");
-  }
-}
